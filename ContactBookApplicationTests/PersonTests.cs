5afe37f4186e2d34c4448e2f5ed13a6b13ba04a7
Alp Aydın <97550808+beatterr@users.noreply.github.com>
2023-10-19 10:22:02 +0300

    Add files via upload

---
+++ ContactBookApplicationTests/PersonTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContactBookApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBookApplication.Tests
+{
+    [TestClass()]
+    public class PersonTests
+    {
+        [TestMethod()]
+        public void getUserLastNameTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
